02b8668a9a149edf1e5247d965f7f106c0b125fe
Charlotte Hearne <charlottehearne.ch@gmail.com>
2022-03-09 13:05:27 +0000

    adding switch for not found pokemon

--- PokemonShakespeare.Api/ClientApp/src/components/Error.js
+++ PokemonShakespeare.Api/ClientApp/src/components/Error.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => (
     <div>
-        <h1>404 - Not Found!</h1>
+        <h1 className="error">Uh oh</h1>
         <p>The Pokemon you searched for doesn't exist :(</p>
     </div>
 );
--- PokemonShakespeare.Api/ClientApp/src/components/SearchBox.js
+++ PokemonShakespeare.Api/ClientApp/src/components/SearchBox.js
@@ -1,22 +1,35 @@
 import React, {useState} from 'react';
 import Error from "./Error";
 
-async function submitSearch(name, f) {
-        const response = await fetch('pokemon/'+name);
+async function submitSearch(name, onSuccess, onFailure) {
+    const response = await fetch('pokemon/' + name);
+    if (response.ok) {
         const data = await response.json();
-        return f(data);
+        onSuccess(data);
+    } else
+        onFailure();
 }
 
 export function PokemonSearcher() {
     const [letters, setLetters] = useState("");
     const [pokemon, setPokemon] = useState({});
+    const [display, setDisplay] = useState("initial");
+    
+    function onSuccess(data){
+        setPokemon(data);
+        setDisplay("success");
+    }
+    
+    function onFailure(){
+        setDisplay("error")
+    }
     
     return <div className="wrap">
             <div className="search">
                 <img id="pokemonLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2880px-International_Pok%C3%A9mon_logo.svg.png"/>
                 <form aria-label="A search box that returns Shakespearean description of a Pokemon"
                       aria-required="true" 
-                      onSubmit={(e) => {submitSearch(letters, setPokemon); e.preventDefault();}}>
+                      onSubmit={(e) => {submitSearch(letters, onSuccess, onFailure); e.preventDefault();}}>
                     <input aria-label="input for pokemon search"
                            aria-required="true" 
                            type="input" 
@@ -25,7 +38,14 @@ export function PokemonSearcher() {
                            onChange={(e) => setLetters(e.target.value)}
                     />
                 </form>
-                <Pokemon {... pokemon}/>
+                {(() => { switch (display) {
+                    case "success":
+                        return <Pokemon {... pokemon}/>;
+                    case "error":
+                        return <Error/>
+                    default:
+                        return <div> </div>
+                }})()}
             </div>
     </div>
 }
